d0ed0449e2d409fca0e23439d3a02140debd3103
杰瑞 <jierui@tongbanjie.com>
2019-11-11 11:29:01 +0800

    Initial commit

---
+++ src/test/java/com/ars/autocode/autocode/AutoCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ars.autocode.autocode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
